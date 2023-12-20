7740b15f7c11562be87d039558cd61cb1d6ac6ab
Eliot <eliot_ye@163.com>
2023-12-20 10:53:45 +0800

    优化 createObjectSchema 类型

--- src/database/schemaType.ts
+++ src/database/schemaType.ts
@@ -1,10 +1,43 @@
-import type {BaseObjectSchema, PropertySchema, PropertyTypeName} from 'realm';
+import type {BaseObjectSchema} from 'realm';
+
+interface PropertySchema<T> {
+  type: T;
+  objectType: SchemaName;
+  property?: string;
+  optional?: boolean;
+  indexed?: boolean | 'full-text';
+  mapTo?: string;
+  default?: unknown;
+}
+
+type BooleanPropertyTypeName = 'bool';
+type NumberPropertyTypeName = 'int' | 'float' | 'double' | 'decimal128';
+type StringPropertyTypeName = 'string' | 'uuid' | 'objectId';
+type DatePropertyTypeName = 'date';
+type OtherPropertyTypeName =
+  | 'object'
+  | 'list'
+  | 'data'
+  | 'linkingObjects'
+  | 'dictionary'
+  | 'set'
+  | 'mixed';
+
+type GetPropertyTypeName<T> = T extends string
+  ? StringPropertyTypeName | PropertySchema<StringPropertyTypeName>
+  : T extends number
+  ? NumberPropertyTypeName | PropertySchema<NumberPropertyTypeName>
+  : T extends boolean
+  ? BooleanPropertyTypeName | PropertySchema<BooleanPropertyTypeName>
+  : T extends Date
+  ? DatePropertyTypeName | PropertySchema<DatePropertyTypeName>
+  : OtherPropertyTypeName | PropertySchema<OtherPropertyTypeName>;
 
 interface Schema<T extends Record<string, any>> {
   name: LSSchemaName | SchemaName;
   primaryKey?: keyof T;
   properties: {
-    [K in keyof T]: PropertyTypeName | PropertySchema;
+    [K in keyof T]: GetPropertyTypeName<T[K]>;
   };
 }
 
