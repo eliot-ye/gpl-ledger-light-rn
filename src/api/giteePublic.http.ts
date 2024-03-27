9402c4f8324c5acced92f0c4502c6fb425ec3dc5
Eliot <eliot_ye@163.com>
2024-03-27 10:59:51 +0800

    change: remove ajv && add @exodus/schemasafe

--- src/api/giteePublic.http.ts
+++ src/api/giteePublic.http.ts
@@ -1,12 +1,10 @@
 import {useMemo} from 'react';
 import {useFetch} from './http';
 import {Platform} from 'react-native';
-// import Ajv from 'ajv';
-// import SVersionList from '../../JSONSchema/public/SVersionList.json';
+import {validator} from '@exodus/schemasafe';
+import SVersionList from '../../JSONSchema/public/SVersionList.json';
 
-// const ajvInstance = new Ajv();
-// const validateVersion = ajvInstance.compile<VersionItem[]>(SVersionList);
-// console.log('SVersionList', JSON.stringify(SVersionList));
+const validateVersion = validator(SVersionList);
 
 export function useApiGiteePublic() {
   const apiGet = useFetch(
@@ -24,17 +22,17 @@ export function useApiGiteePublic() {
       /** 获取最新版本 */
       async nowVersion(): Promise<VersionItem[]> {
         const res = await apiGet('/now_version.json');
-        // if (!ajvInstance.validate(SVersionList, res)) {
-        //   return Promise.reject(new Error('数据校验失败'));
-        // }
+        if (!validateVersion(res)) {
+          return Promise.reject(new Error('数据校验失败'));
+        }
         return res;
       },
       /** 获取版本更新记录 */
       async versionLog(): Promise<VersionItem[]> {
         const res = await apiGet('/change_log.json');
-        // if (!validateVersion(res)) {
-        //   return Promise.reject(new Error('数据校验失败'));
-        // }
+        if (!validateVersion(res)) {
+          return Promise.reject(new Error('数据校验失败'));
+        }
         return res;
       },
     }),
