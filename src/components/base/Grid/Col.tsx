fd49b31e576976c046e1c3a29e020dcd8478951f
Eliot <eliot_ye@163.com>
2024-08-05 11:17:40 +0800

    fix: Col & breakpointWidth

--- src/components/base/Grid/Col.tsx
+++ src/components/base/Grid/Col.tsx
@@ -1,9 +1,9 @@
-import React, {memo, useContext, useMemo} from 'react';
+import {memo, useContext, useMemo} from 'react';
 import {DimensionValue, View, ViewProps} from 'react-native';
 import {RowContext} from './Row';
 import {CPNText} from '..';
 import {Colors} from '@/assets/colors';
-import {breakpointWidth} from '@/utils/dimensions';
+import {BreakpointWidthSelectOptions, WindowSize} from '@/utils/dimensions';
 
 const BASE_SPAN_WIDTH = 100 / 24;
 
@@ -39,53 +39,34 @@ interface ColProps extends ViewProps {
    * 单元格默认宽度比例
    * @default 24
    */
-  span?: ColSpan;
-  /**
-   * 单元格宽度，Row 组件的宽度大于等于 520 时生效
-   */
-  span_md?: ColSpan;
-  /**
-   * 单元格宽度，Row 组件的宽度大于等于 840 时生效
-   */
-  span_lg?: ColSpan;
+  span?: ColSpan | BreakpointWidthSelectOptions<ColSpan>;
   /**
    * 单元格默认偏移量
    * @default 0
    */
-  offset?: ColSpan;
-  /**
-   * 单元格偏移量，Row 组件的宽度大于等于 520 时生效
-   */
-  offset_md?: ColSpan;
-  /**
-   * 单元格偏移量，Row 组件的宽度大于等于 840 时生效
-   */
-  offset_lg?: ColSpan;
+  offset?: ColSpan | BreakpointWidthSelectOptions<ColSpan>;
 }
 
 function Col(props: Readonly<ColProps>) {
   const {gap, rowWidth, isInRow} = useContext(RowContext);
 
   const width: DimensionValue = useMemo<DimensionValue>(() => {
-    if (rowWidth >= breakpointWidth.lg && props.span_lg !== undefined) {
-      return `${props.span_lg * BASE_SPAN_WIDTH}%`;
-    } else if (rowWidth >= breakpointWidth.md && props.span_md !== undefined) {
-      return `${props.span_md * BASE_SPAN_WIDTH}%`;
+    if (typeof props.span === 'number') {
+      return `${props.span * BASE_SPAN_WIDTH}%`;
+    } else {
+      const _ = WindowSize.select({default: 24, ...props.span}, rowWidth);
+      return `${_ * BASE_SPAN_WIDTH}%`;
     }
-    return `${(props.span ?? 24) * BASE_SPAN_WIDTH}%`;
-  }, [props.span, props.span_md, props.span_lg, rowWidth]);
+  }, [props.span, rowWidth]);
 
   const marginStart: DimensionValue = useMemo<DimensionValue>(() => {
-    if (rowWidth >= breakpointWidth.lg && props.offset_lg !== undefined) {
-      return `${props.offset_lg * BASE_SPAN_WIDTH}%`;
-    } else if (
-      rowWidth >= breakpointWidth.md &&
-      props.offset_md !== undefined
-    ) {
-      return `${props.offset_md * BASE_SPAN_WIDTH}%`;
+    if (typeof props.offset === 'number') {
+      return `${props.offset * BASE_SPAN_WIDTH}%`;
+    } else {
+      const _ = WindowSize.select({default: 0, ...props.offset}, rowWidth);
+      return `${_ * BASE_SPAN_WIDTH}%`;
     }
-    return `${(props.offset ?? 0) * BASE_SPAN_WIDTH}%`;
-  }, [props.offset, props.offset_md, props.offset_lg, rowWidth]);
+  }, [props.offset, rowWidth]);
 
   if (!isInRow) {
     return (
--- src/utils/dimensions.ts
+++ src/utils/dimensions.ts
@@ -1,11 +1,6 @@
 import {useEffect, useState} from 'react';
 import {Dimensions} from 'react-native';
 
-export const breakpointWidth = {
-  md: 520,
-  lg: 840,
-} as const;
-
 export function useDimensions(type: 'window' | 'screen') {
   const [scaledSize, scaledSizeSet] = useState(Dimensions.get(type));
 
@@ -24,3 +19,74 @@ export function useDimensions(type: 'window' | 'screen') {
 
   return scaledSize;
 }
+
+const breakpointWidth = {
+  default: 0,
+  xs: 480,
+  sm: 576,
+  md: 768,
+  lg: 992,
+  xl: 1200,
+  xxl: 1600,
+} as const;
+
+export type BreakpointWidthSelectOptions<T> = {
+  [key in keyof typeof breakpointWidth]?: T;
+};
+export type BreakpointWidthSelectOptionsWidthDefault<T> =
+  BreakpointWidthSelectOptions<T> & {default: T};
+
+export const WindowSize = {
+  useDimensions,
+  breakpointWidth,
+  /**
+   * 根据宽度选择不同的数据，默认使用 `default` 值
+   * @param option
+   * @returns
+   */
+  select<T>(
+    option: BreakpointWidthSelectOptionsWidthDefault<T>,
+    width?: number,
+  ) {
+    const clientWidth = width ?? Dimensions.get('window').width;
+
+    if (clientWidth > breakpointWidth.xxl && option.xxl !== undefined) {
+      return option.xxl;
+    } else if (clientWidth > breakpointWidth.xl && option.xl !== undefined) {
+      return option.xl;
+    } else if (clientWidth > breakpointWidth.lg && option.lg !== undefined) {
+      return option.lg;
+    } else if (clientWidth > breakpointWidth.md && option.md !== undefined) {
+      return option.md;
+    } else if (clientWidth > breakpointWidth.sm && option.sm !== undefined) {
+      return option.sm;
+    } else if (clientWidth > breakpointWidth.xs && option.xs !== undefined) {
+      return option.xs;
+    }
+
+    return option.default;
+  },
+
+  /**
+   * 根据屏幕宽度选择不同的数据，默认使用 `default` 值
+   * @param option
+   * @returns
+   */
+  useSelect<T>(option: BreakpointWidthSelectOptionsWidthDefault<T>) {
+    const [data, setData] = useState(option.default);
+
+    useEffect(() => {
+      setData(WindowSize.select(option));
+
+      const ev = Dimensions.addEventListener('change', ({window}) => {
+        setData(WindowSize.select(option, window.width));
+      });
+
+      return () => {
+        ev.remove();
+      };
+    }, []);
+
+    return data;
+  },
+} as const;
